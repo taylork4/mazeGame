20616a1d60e6fe99cc86bdada255f7d24afce3e1
Cameron Snoap <snoapca@mail.gvsu.edu>
2023-02-01 08:52:46 -0500

    class time progress

--- python_game.py
+++ python_game.py
@@ -1,3 +1,30 @@
+'''
+***********************************************************************************************************************************
+Has a core game loop that is frame limited (10)
+***********************************************************************************************************************************
+Must have some mechanism for easily changing the framerate (be it a run time option, or a simple easy to change final variable) (10)
+***********************************************************************************************************************************
+Grow and draw a maze using the prescribed cellular automata method (15)
+***********************************************************************************************************************************
+Draw your maze at the framerate using squares for each cell (10)
+***********************************************************************************************************************************
+Must handle quit events, mouse events, and keyboard events. Clicking the close button of the game window should gracefully shutdown the game. 
+Clicking on a rectangle causes that rectangle to either become a new random color (if it is currently (0, 0, 0)), or become (0, 0, 0) if it has any color at all. 
+Note that (0, 0, 0) is an open cell and can be traversed. Any other color is a wall. (15)
+***********************************************************************************************************************************
+Must have a circle that represents the player. The player will start at the top left corner of the maze. 
+If there is currently a cell there, remove it before adding the player. 
+The player must be able to move through the maze corridors, but cannot go through walls, attempting to get to the lower right corner. 
+Making it to the lower right corner should result in a "win". 
+Keep in mind that mouse clicks can turn on and off a wall; this is not intended to be a fun game, merely a project to get the absolute basics working before we move forward with more complicated coding (20)
+***********************************************************************************************************************************
+Fully commented and documented code that includes headers for all public classes and methods. 
+If a method has a return type, or takes parameters, or throws exceptions, it should be documented. 
+Use the Google Style Guide (https://google.github.io/styleguide/) when you have questions. (10)
+***********************************************************************************************************************************
+You must demo your project in class on the due date for full points (10)
+'''
+
 import pygame
 
 # Initialize pygame
@@ -8,53 +35,66 @@ screen = pygame.display.set_mode((500, 500))
 
 # Set the red of the rectangle
 red = (255, 0, 0)
+black = (0, 0, 0)
+# sqr = pygame.Rect(0, 0, 20, 20)
+# def grid(l, t, w, h):
+
+
 
 # Create a rectangle with the size 10x10 at the position (100, 100)
-rect = pygame.Rect(100, 100, 10, 10)
+rect = pygame.Rect(10, 10, 10, 10)
 
 # Move the rectangle with the arrow keys
 def handle_keys():
+    keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        if rect.y > 5:
-            rect.y -= 1
+        if rect.y > 10:
+            rect.y -= 20
     if keys[pygame.K_DOWN]:
-        if rect.y < 495:
-            rect.y += 1
+        if rect.y < 490:
+            rect.y += 20
     if keys[pygame.K_LEFT]:
-        if rect.x > 5:
-            rect.x -= 1
+        if rect.x > 10:
+            rect.x -= 20
     if keys[pygame.K_RIGHT]:
-        if rect.x < 495:
-            rect.x += 1
+        if rect.x < 490:
+            rect.x += 20
 
 # Set the frame rate
 clock = pygame.time.Clock()
-frame_rate = 200
+frame_rate = 10
 
+def gameloop():
 # Run the game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    # Get the pressed keys
-    keys = pygame.key.get_pressed()
+        handle_keys()
 
-    handle_keys()
+        # Clear the screen
+        screen.fill((255, 255, 255))
 
-    # Clear the screen
-    screen.fill((255, 255, 255))
+        # Draw the rectangle on the screen
+        # pygame.draw.rect(screen, red, (0,0), 5, 0)
+        
+        #draw player circle on the screen
+        pygame.draw.circle(screen, red, (rect.x, rect.y), 10, 0)
 
-    # Draw the rectangle on the screen
-    # pygame.draw.rect(screen, red, rect)
-    pygame.draw.circle(screen, red, (rect.x, rect.y), 5, 0)
+        # Update the screen
+        pygame.display.update()
 
-    # Update the screen
-    pygame.display.update()
+        # Limit the frame rate
+        clock.tick(frame_rate)
 
-    # Limit the frame rate
-    clock.tick(frame_rate)
+    # Quit pygame
+    pygame.quit()
 
-# Quit pygame
-pygame.quit()
+def main():
+
+    gameloop()
+
+if (__name__ == "__main__"):
+    main()
